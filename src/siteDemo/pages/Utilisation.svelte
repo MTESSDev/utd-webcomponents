a719a2b0c5f5bca3b644cfb04b9e0912a0226976
Christian Caux <Christian.Caux@mtess.gouv.qc.ca>
2022-05-31 12:38:17 -0400

    v1.5.0a

--- src/siteDemo/pages/Utilisation.svelte
+++ src/siteDemo/pages/Utilisation.svelte
@@ -5,23 +5,40 @@
 <h1>Utilisation</h1>
 
 <p>Pour utiliser les composants utd, il suffit d'injecter un fichier javascript, un fichier css, un fichier svg et quelques fichiers de fonts.</p>
-<p>Vous pouvez charger les fichiers directement à partir du CDN xyz ou encore en rappatriant les fichiers dans votre solution afin de pouvoir les fusionner avec les vôtres histoire de diminuer le nombre de fichiers.</p>
-<p>Attention, si vous copiez les fichiers dans votre projet, les fichiers doivent cependant être dans un dossier avec la structure suivante :</p>
 
 
 
-<h2>Intégration dans un projet</h2>
+<h2>Intégration dans un projet via CDN</h2>
 <h3>Chargement du javascript</h3>
-<p>Charger le script à la fin du body de la page. Ici on présente le lien du CDN mais vous pouvez pointer sur votre copie le cas échéant.</p>
-<CodeSource codeSource="<script src='https://cdn.jsdelivr.net/gh/MTESSDev/utd-webcomponents@1.4.1/dist/js/utd-webcomponents.min.js'></script>">
+<p>Charger le script à la fin du <span class="utd-emphase">body</span> de la page.</p>
+<CodeSource codeSource="<script src='https://cdn.jsdelivr.net/gh/MTESSDev/utd-webcomponents@1.5.0a/dist/js/utd-webcomponents.min.js'></script>">
 </CodeSource>   
 
 <h3>Chargement du css</h3>
-<p>Charger le css dans la balise head après le css de votre projet. Ici on présente le lien du CDN mais vous pouvez pointer sur votre copie le cas échéant.</p>
-<CodeSource codeSource="<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/MTESSDev/utd-webcomponents@1.4.1/dist/css/utd-webcomponents.min.css'>">
+<p>Charger le css dans la balise <span class="utd-emphase">head</span> après le css de votre projet.</p>
+<CodeSource codeSource="<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/MTESSDev/utd-webcomponents@1.5.0a/dist/css/utd-webcomponents.min.css'>">
 </CodeSource>   
 
-<h3>Chargement des fonts</h3>
+<h2>Intégration dans un projet (Copie locale des fichiers)</h2>
+<p>Vous pouvez rappatrier les fichiers dans votre solution afin de pouvoir les fusionner avec les vôtres histoire de diminuer le nombre de fichiers.</p>
+<p>Attention, si vous copiez les fichiers dans votre projet, les fichiers doivent cependant respecter la structure définie dans de dossier "dest" disponible dans la release.</p>
+
+<p><a href="https://github.com/MTESSDev/utd-webcomponents/releases" target="_blank">Consulter les releases et télécharger les fichiers requis</a></p>
+
+
+<h3>Chargement du javascript</h3>
+<p>Charger le script à la fin du <span class="utd-emphase">body</span> de la page.</p>
+<CodeSource codeSource="<script src='/js/utd-webcomponents.min.js'></script>">
+</CodeSource>   
+
+<h3>Chargement du css</h3>
+<p>Charger le css dans la balise <span class="utd-emphase">head</span> après le css de votre projet.</p>
+<CodeSource codeSource="<link rel='stylesheet' href='/css/utd-webcomponents.min.css'>">
+</CodeSource>
+
+
+
+
 
 <style>
 </style>
