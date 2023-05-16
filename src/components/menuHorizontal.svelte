205d00c4933216cec01552f8f508d96b7d2eeb8e
j0ber <chriscaux@hotmail.com>
2023-05-16 15:47:11 -0400

    WIP (avant passage v1.12.0)

--- src/components/menuHorizontal.svelte
+++ src/components/menuHorizontal.svelte
@@ -12,10 +12,14 @@
   const idTitreMenu = Utils.genererId()
   const srTexteSortirMenu = Utils.obtenirLanguePage() === "en" ?  "Press ESC key to exit menu." : "Appuyez sur la touche Échappe pour sortir du menu."
   const thisComponent = get_current_component()
-  let controleMenu 
+  const paddingHorizontal = 16
+  const margeDroite = 32
+  let controleMenu
+  let controleMenuItemPlus 
   let largeurConteneur = 0
   let largeurMenu = 0
   let menuOriginal = []
+  let dernierIndexeVisible = 0
   // Références pour accessibilité
   // https://www.w3.org/WAI/ARIA/apg/patterns/disclosure/examples/disclosure-navigation/
 
@@ -36,16 +40,45 @@
 
   onMount(() => {      
     controleMenu = thisComponent.shadowRoot.getElementById(idMenu)
-    //test()
 
     setTimeout(() => {
-      test()
-      contientMenusNonVisibles()      
-//      console.log(menuOriginal)
-    }, 1000)
 
-    Utils.reafficherApresChargement(thisComponent)
+      if(contientMenusNonVisibles()) {        
+        ajouterMenuPlusTemporaire()
+          setTimeout(() => {
+            //On enlève 1, car notre menuPlus temporaire ne doit pas compter
+            dernierIndexeVisible = obtenirDernierIndexeVisible() - 1          
+            thisComponent.children[0].remove()
+//            ajouterMenuPlus()
+
+            masquerMenusExcedentaires()
+            console.log('dernier indexe visible -> ' + dernierIndexeVisible)          
+            setTimeout(() => {
+              Utils.reafficherApresChargement(thisComponent)              
+            }, 200)
+          },100)
+      } else {
+        console.log('Le menu fit, rien à faire!')
+        ajouterMenuPlus()
+        Utils.reafficherApresChargement(thisComponent)
+      }
+
+      console.log(menuOriginal)
+    })
   })
+
+  function masquerMenusExcedentaires() {
+    const menuPlus = document.createElement('utd-menu-horizontal-item')
+    menuPlus.setAttribute('libelle', 'Plus')
+
+    for (let i = dernierIndexeVisible + 1; i < thisComponent.children.length; i++) {
+      const cln = thisComponent.children[i].cloneNode(true);
+      menuPlus.appendChild(cln)            
+      thisComponent.children[i].classList.add('utd-d-none')      
+    }
+
+    thisComponent.appendChild(menuPlus)    
+  }
 
   function ajouterElementsMenuAuMenuOriginal(elementMenuItem, parent) {
     Array.from(parent.children).forEach((child) => {
@@ -68,54 +101,60 @@
     afficher = !afficher
   }
 
-  function contientMenusNonVisibles(){
-    largeurConteneur = thisComponent.getBoundingClientRect().width
+  function contientMenusNonVisibles() {
 
-
-//    console.log('Largeur conteneur -> ' + largeurConteneur)
-//    console.log('Largeur menu -> ' + largeurMenu)
-
-  }
-
-  function test(){
-    let menuVisible = true
-    let i = 0
-    let dernierIndexeVisible = 0
-
-    largeurConteneur = thisComponent.getBoundingClientRect().right
+    largeurConteneur = thisComponent.getBoundingClientRect().right - paddingHorizontal
     largeurMenu = thisComponent.children[thisComponent.children.length - 1].getBoundingClientRect().right
 
     //console.log(largeurMenu)
-    if(largeurMenu <= largeurConteneur) {
-      //console.log('Le menu fit, rien à faire!')
-    }
+    return largeurMenu > largeurConteneur
+  }
 
+  function obtenirDernierIndexeVisible(){
+    let menuVisible = true
+    let i = 0
+    let indexeDernierElementVisible = 0
+
+    const menuPlus = thisComponent.children[thisComponent.children.length - 1]
+    const largeurMenuPlus = menuPlus.getBoundingClientRect().right - menuPlus.getBoundingClientRect().left
+    console.log('Largeur menu plus -> ' + largeurMenuPlus)
     while (menuVisible && i <= thisComponent.children.length - 1) {
       const item = thisComponent.children[i]
 
-//      largeurMenu += item.getBoundingClientRect().right
-      //console.log('indexe = ' + i + '    right = ' + item.getBoundingClientRect().right)
-      if(item.getBoundingClientRect().right <= largeurConteneur){
-        dernierIndexeVisible = i
+      if((item.getBoundingClientRect().right + largeurMenuPlus) <= largeurConteneur){
+        indexeDernierElementVisible = i
         i++        
       } else {
         menuVisible = false
       }      
     }
-//      return indexeDernierMenuVisible
-    //console.log(dernierIndexeVisible)
+
+    return indexeDernierElementVisible   
   }
 
-  function test9(){
+  function ajouterMenuPlusTemporaire(){
+    const menuPlusTemp = document.createElement('utd-menu-horizontal-item')
+    menuPlusTemp.setAttribute('libelle', 'Plus')
+
+    const elementTemp = document.createElement('utd-menu-horizontal-item')
+    elementTemp.setAttribute('libelle', 'Temporaire')
+    menuPlusTemp.appendChild(elementTemp)
+
+    thisComponent.prepend(menuPlusTemp)
+    controleMenuItemPlus = menuPlusTemp
+  }
+
+  function ajouterMenuPlus(){
     const menuPlus = document.createElement('utd-menu-horizontal-item')
     menuPlus.setAttribute('libelle', 'Plus')
       
     thisComponent.childNodes.forEach(function(item){
       const cln = item.cloneNode(true);
-//      menuPlus.appendChild(cln);
+      menuPlus.appendChild(cln);
     })
 
-//    thisComponent.appendChild(menuPlus)
+    thisComponent.appendChild(menuPlus)
+    controleMenuItemPlus = menuPlus
   }
 
 </script>
